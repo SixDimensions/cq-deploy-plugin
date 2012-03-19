835c906d06d6754654fe087f9ab45059e989d06b
dan.klco <dan.klco@188c018c-155f-4d0b-a3fe-d0394b648509>
2012-03-19 02:41:50 +0000

    Starting to work on the package Mojo

--- cq-package-plugin/src/main/java/com/sixdimensions/wcm/cq/pack/PackageMojo.java
+++ cq-package-plugin/src/main/java/com/sixdimensions/wcm/cq/pack/PackageMojo.java
@@ -1,20 +1,22 @@
-package com.sixdimensions.wcm.cq.pack;
-
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2012 - Six Dimensions
+ * 
+ * This file is part of the CQ Package Plugin.
+ * 
+ * The CQ Package Plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The CQ Package Plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with the CQ Package Plugin.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.sixdimensions.wcm.cq.pack;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -26,60 +28,53 @@ import org.apache.maven.plugin.MojoExecutionException;
 import com.sixdimensions.wcm.cq.pack.service.PackageManagerService;
 
 /**
- * Goal which touches a timestamp file.
- *
- * @goal touch
+ * Goal which uploads and installs a package to Adobe CQ.
  * 
- * @phase process-sources
+ * @goal install-package
+ * 
+ * @phase install
  */
-public class PackageMojo extends AbstractMojo
-{
-    /**
-     * Location of the file.
-     * @parameter expression="${project.build.directory}"
-     * @required
-     */
-    private File outputDirectory;
-    
-    
+public class PackageMojo extends AbstractMojo {
+	/**
+	 * Location of the file.
+	 * 
+	 * @parameter expression="${project.build.directory}"
+	 * @required
+	 */
+	private File outputDirectory;
+	
+	/**
+	 * Location of the file.
+	 * 
+	 * @parameter expression="${project.build.directory}"
+	 */
+	private String host;
 
-    public void execute()
-        throws MojoExecutionException{
-        File f = outputDirectory;
-        PackageManagerService.Factory.getPackageMgr(null);
+	public void execute() throws MojoExecutionException {
+		File f = outputDirectory;
+		PackageManagerService.Factory.getPackageMgr(null);
 
-        if ( !f.exists() )
-        {
-            f.mkdirs();
-        }
+		if (!f.exists()) {
+			f.mkdirs();
+		}
 
-        
-        File touch = new File( f, "touch.txt" );
+		File touch = new File(f, "touch.txt");
 
-        FileWriter w = null;
-        try
-        {
-            w = new FileWriter( touch );
+		FileWriter w = null;
+		try {
+			w = new FileWriter(touch);
 
-            w.write( "touch.txt" );
-        }
-        catch ( IOException e )
-        {
-            throw new MojoExecutionException( "Error creating file " + touch, e );
-        }
-        finally
-        {
-            if ( w != null )
-            {
-                try
-                {
-                    w.close();
-                }
-                catch ( IOException e )
-                {
-                    // ignore
-                }
-            }
-        }
-    }
+			w.write("touch.txt");
+		} catch (IOException e) {
+			throw new MojoExecutionException("Error creating file " + touch, e);
+		} finally {
+			if (w != null) {
+				try {
+					w.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+		}
+	}
 }
