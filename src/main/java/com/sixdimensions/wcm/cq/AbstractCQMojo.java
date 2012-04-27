04812c1d85b5a0c24425cdaf9ce6ae69350f3ce3
dan.klco <dan.klco@188c018c-155f-4d0b-a3fe-d0394b648509>
2012-04-27 20:50:17 +0000

    Changed plugin name in comments and added javadocs into the Mojo

--- src/main/java/com/sixdimensions/wcm/cq/AbstractCQMojo.java
+++ src/main/java/com/sixdimensions/wcm/cq/AbstractCQMojo.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2012 - Six Dimensions
  * 
- * This file is part of the CQ Package Plugin.
+ * This file is part of the CQ Deploy Plugin.
  * 
- * The CQ Package Plugin is free software: you can redistribute it and/or modify
+ * The CQ Deploy Plugin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * The CQ Package Plugin is distributed in the hope that it will be useful,
+ * The CQ Deploy Plugin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with the CQ Package Plugin.  If not, see <http://www.gnu.org/licenses/>.
+ * along with the CQ Deploy Plugin.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.sixdimensions.wcm.cq;
 
@@ -53,7 +53,7 @@ public abstract class AbstractCQMojo extends AbstractMojo {
 	protected String password;
 
 	/**
-	 * The port of the server to connect to. Default is 'admin'.
+	 * The port of the server to connect to. Default is '4502'.
 	 * 
 	 * @parameter default-value="4502"
 	 */
@@ -66,19 +66,40 @@ public abstract class AbstractCQMojo extends AbstractMojo {
 	 */
 	protected String user;
 
-	public String getHost() {
+	/**
+	 * Retrieves host of the server to connect to, including protocol. Default
+	 * is 'http://localhost'.
+	 * 
+	 * @return the host
+	 */
+	public final String getHost() {
 		return host;
 	}
 
-	public String getPassword() {
+	/**
+	 * Retrieves the password to use when connecting. Default is 'admin'.
+	 * 
+	 * @return the password
+	 */
+	public final String getPassword() {
 		return password;
 	}
 
-	public String getPort() {
+	/**
+	 * The port of the server to connect to. Default is '4502'.
+	 * 
+	 * @return the server port to connect to
+	 */
+	public final String getPort() {
 		return port;
 	}
 
-	public String getUser() {
+	/**
+	 * The username to use when connecting. Default is 'admin'.
+	 * 
+	 * @return the username
+	 */
+	public final String getUser() {
 		return user;
 	}
 
@@ -100,26 +121,65 @@ public abstract class AbstractCQMojo extends AbstractMojo {
 		config.setUser(user);
 	}
 
-	public boolean isErrorOnFailure() {
+	/**
+	 * Flag to determine whether or not to quit and throw an error when an API
+	 * call fails. Default is true.
+	 * 
+	 * @return the error on failure flag
+	 */
+	public final boolean isErrorOnFailure() {
 		return errorOnFailure;
 	}
 
-	public void setErrorOnFailure(boolean errorOnFailure) {
+	/**
+	 * Sets the flag to determine whether or not to quit and throw an error when
+	 * an API call fails.
+	 * 
+	 * @param errorOnFailure
+	 *            the error on failure flag
+	 */
+	public final void setErrorOnFailure(boolean errorOnFailure) {
 		this.errorOnFailure = errorOnFailure;
 	}
 
+	/**
+	 * Sets the host of the server to connect to, must include the protocol,
+	 * must not be null.
+	 * 
+	 * @param host
+	 *            the host to connect to
+	 */
 	public void setHost(String host) {
 		this.host = host;
 	}
 
+	/**
+	 * Sets the password to use when connecting, must not be null.
+	 * 
+	 * @param password
+	 *            the password
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	/**
+	 * The port of the server to connect to, this should always be set, even if
+	 * it's a standard port for the protocol (ex: 80).
+	 * 
+	 * @param port
+	 *            the port to connect to
+	 */
 	public void setPort(String port) {
 		this.port = port;
 	}
 
+	/**
+	 * Sets the username to use when connecting, must not be null.
+	 * 
+	 * @param user
+	 *            the username
+	 */
 	public void setUser(String user) {
 		this.user = user;
 	}
--- src/main/java/com/sixdimensions/wcm/cq/pack/PackageMojo.java
+++ src/main/java/com/sixdimensions/wcm/cq/pack/PackageMojo.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2012 - Six Dimensions
  * 
- * This file is part of the CQ Package Plugin.
+ * This file is part of the CQ Deploy Plugin.
  * 
- * The CQ Package Plugin is free software: you can redistribute it and/or modify
+ * The CQ Deploy Plugin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * The CQ Package Plugin is distributed in the hope that it will be useful,
+ * The CQ Deploy Plugin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with the CQ Package Plugin.  If not, see <http://www.gnu.org/licenses/>.
+ * along with the CQ Deploy Plugin.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.sixdimensions.wcm.cq.pack;
 
