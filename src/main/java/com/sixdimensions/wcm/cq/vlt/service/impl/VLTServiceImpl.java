64aab7f63d29df4223b56da3dd2598ef7e5eaa7e
dan.klco <dan.klco@188c018c-155f-4d0b-a3fe-d0394b648509>
2012-06-01 13:19:32 +0000

    Installing framework for having a VLT based import process

---
+++ src/main/java/com/sixdimensions/wcm/cq/vlt/service/impl/VLTServiceImpl.java
@@ -0,0 +1,12 @@
+package com.sixdimensions.wcm.cq.vlt.service.impl;
+
+import com.sixdimensions.wcm.cq.vlt.CQVLTServiceConfig;
+import com.sixdimensions.wcm.cq.vlt.service.VLTService;
+
+public class VLTServiceImpl implements VLTService {
+
+	public VLTServiceImpl(CQVLTServiceConfig config) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
